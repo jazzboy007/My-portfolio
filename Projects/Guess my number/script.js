04380e04ef15f9a68347af23b38cc8ce1e90d2e3
jazzboy007 <36272693+jazzboy007@users.noreply.github.com>
2024-10-10 03:53:56 +0100

    Add files via upload

---
+++ Projects/Guess my number/script.js
@@ -0,0 +1,67 @@
+// DOM: Document object model: Structured representation of HTML document.
+// Allows JS to Access HTML element and stylesto manipulate them
+
+'use strict';
+
+/*console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = 'Correct Number!';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);*/
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+let score = 20;
+let highscore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(typeof guess);
+  // when there is no input
+  if (!guess) {
+    displayMessage('📛No Number!');
+    // When player wins
+  } else if (guess === secretNumber) {
+    displayMessage('Correct Number!');
+    document.querySelector('.number').textContent = secretNumber;
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+    // when guess is wrong
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? '💹Too high' : '📉Too Low');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('😢you lost the game');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+
+  //
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  displayMessage('Start Guessing...');
+  document.querySelector('.score').textContent = score;
+
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+});
